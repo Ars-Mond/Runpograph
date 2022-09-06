452f57b697085be3b35ec9ba88a9b57977bda183
Ars_Mond <ars123.www@gmail.com>
2022-09-06 16:49:01 +0500

    feat (core):  Modified delegate command

--- Core/Commands/DelegateCommand.cs
+++ Core/Commands/DelegateCommand.cs
@@ -4,22 +4,39 @@ namespace Core.Commands;
 
 public class DelegateCommand : ICommand
 {
-    private readonly Action<object> _open;
+    public event Action NullExecute = null!;
+    
+    private readonly Action<object> _execute;
+    private readonly Func<bool>? _canExecute;
 
-    public DelegateCommand(Action<object> open)
+    public DelegateCommand(Action<object> execute)
     {
-        _open = open;
+        _execute = execute ?? throw new NullReferenceException(nameof(execute));
+    }
+    
+    public DelegateCommand(Action<object> execute, Func<bool> canExecute) : this(execute)
+    {
+        _canExecute = canExecute;
+    }
+
+    private void OnNullExecute()
+    {
+        NullExecute?.Invoke();
     }
 
     public bool CanExecute(object? parameter)
     {
-        return true;
+        return _canExecute == null || _canExecute();
     }
 
     public void Execute(object? parameter)
     {
-        //if (parameter == null) return;
-        _open?.Invoke(parameter);
+        if (parameter == null)
+        {
+            OnNullExecute();
+            return;
+        }
+        _execute?.Invoke(parameter);
     }
 
     public event EventHandler? CanExecuteChanged;
