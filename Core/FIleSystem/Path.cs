f2e2993fb5dcb81ba968d9ef16505fb372f3863c
Ars_Mond <ars123.www@gmail.com>
2022-09-06 16:53:38 +0500

    feat (core): Add path class in file system

---
+++ Core/FIleSystem/Path.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.FileSystem;
+
+public class Path
+{
+    protected string _path;
+
+    public string Current => _path;
+
+    public Path(string path)
+    {
+        _path = path;
+    }
+
+    public Path(DirectoryInfo directory)
+    {
+        _path = directory.FullName;
+    }
+
+    public Path(FileInfo file)
+    {
+        _path = file.FullName;
+    }
+
+    
+}
