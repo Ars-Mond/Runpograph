92eaca00ee921989d50ec6963843a3e5f674c186
Ars_Mond <ars123.www@gmail.com>
2022-09-06 16:55:56 +0500

    feat (core): Add editor system

---
+++ Core/Editor/DocumentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Path = Core.FileSystem.Path;
+
+namespace Core.Editor;
+
+public sealed class DocumentViewModel : BaseViewModel
+{
+    public string Text { get; set; }
+
+    public string FileName { get; set; }
+    public Path FilePath { get; set; }
+
+    public bool isEmpty => string.IsNullOrEmpty(FileName) || string.IsNullOrEmpty(FilePath.Current);
+
+    public DocumentViewModel(Path filePath, string fileName, string? text)
+    {
+        FilePath = filePath;
+        FileName = fileName;
+        Text = text ?? string.Empty;
+    }
+    
+    public DocumentViewModel(string filePath, string fileName, string? text)
+    {
+        FilePath = new Path(filePath);
+        FileName = fileName;
+        Text = text ?? string.Empty;
+    }
+
+    
+    
+    
+    
+}
---
+++ Core/Editor/FormatViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Media;
+
+namespace Core.Editor;
+
+public sealed class FormatViewModel : BaseViewModel
+{
+    public FontFamily Family { get; set; }
+    
+    public FontStyle Style { get; set; }
+
+    public FontWeight Weight { get; set; }
+
+    private TextWrapping _wrap;
+
+    public TextWrapping Wrap
+    {
+        get => _wrap;
+        set => isWrapped = value == TextWrapping.Wrap;
+    }
+
+    public bool isWrapped { get; set; }
+    
+    public double SizeFont { get; set; }
+}
